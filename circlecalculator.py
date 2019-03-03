737e82c9bcde8bbb9baf222fed773827d046e10d
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-03-02 19:09:27 -0500

    Add files via upload

---
+++ circlecalculator.py
@@ -0,0 +1,10 @@
+# Created by: Aden Rao
+# Created on: Febuary 25th, 2019 
+# This program you put in the diameter of a circle and it will calculate the area and circumference of the circle.
+
+diameter = int(input( ' enter the diameter: '))
+
+import math
+
+print (round(math.pi*(diameter/2)*(diameter/2)))
+print (round(math.pi*(diameter)))
